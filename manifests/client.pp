68fdb9d19616fc00b65e7cb62f7b7ff91a654461
primeministerp <primeministerpete@gmail.com>
2014-06-18 14:04:11 -0400

    updating repo to match production

---
+++ manifests/client.pp
@@ -0,0 +1,47 @@
+class sensu_server::client {
+#file {'/etc/sensu/ssl/cacert.pem':
+#    ensure  => present,
+#    source  => 'puppet:///extra_files/sensu/cacert.pem',
+#    require => File['/etc/sensu/ssl'],
+#    owner   => 'sensu',
+#    group   => 'sensu',
+#    mode    => '0644',
+#  }
+#  file {'/etc/sensu/ssl/cert.pem':
+#    ensure  => present,
+#    source  => 'puppet:///extra_files/sensu/cacert.pem',
+#    require => File['/etc/sensu/ssl'],
+#    owner   => 'sensu',
+#    group   => 'sensu',
+#    mode    => '0644',
+#  }
+#  file {'/etc/sensu/ssl/key.pem':
+#    ensure => present,
+#    source  => 'puppet:///extra_files/sensu/cacert.pem',
+#    require => File['/etc/sensu/ssl'],
+#    owner   => 'sensu',
+#    group   => 'sensu',
+#    mode    => '0644',
+#  }
+
+  class{'sensu':
+#    rabbitmq_password        => $sensu_client::rabbitmq_password,
+#    rabbitmq_host            => "${sensu::rabbitmq_host}",
+#    rabbitmq_ssl_cert_chain  => "${sensu::rabbitmq_ssl_cert_chain}",
+#    rabbitmq_ssl_private_key => "${sensu::rabbitmq_ssl_private_key}",
+    subscriptions            => $sensu::subscriptions,
+#    dashboard                => $sensu::dashboard,
+#    server                   => $sensu::server,
+#    api                      => $sensu::api,
+    rabbitmq_password        => 'sensu',
+    rabbitmq_host            => '10.21.7.4',
+    rabbitmq_ssl_cert_chain  => 'puppet:///extra_files/sensu/cert.pem',
+    rabbitmq_ssl_private_key => 'puppet:///extra_files/sensu/key.pem',
+#    subscriptions            => 'production',
+    dashboard                => false,
+    server                   => false,
+    api                      => false,
+#    safe_mode => true,
+  }
+
+}
---
+++ manifests/email_handler.pp
@@ -0,0 +1,74 @@
+# == Class: sensu_server::email_handler
+#
+# Full description of class sensu_server here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { sensu_server:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2014 Your name here, unless otherwise noted.
+#
+class sensu_server::email_handler (
+  $mail_from        = 'sensu@example.com',
+  $mail_to          = 'monitor@example.com',
+  $delivery_method  = 'smtp',
+  $smtp_address     = 'localhost',
+  $smtp_port        = 25,
+  $smtp_domain      = 'localhost.localdomain',
+  $smtp_username    = undef,
+  $smtp_password    = undef,
+  $smtp_auth        = undef,
+  $smtp_enable_tls  = undef,
+){
+
+  sensu::handler{'mailer':
+    type => 'pipe',
+    command => '/opt/sensu/embedded/bin/ruby /etc/sensu/plugins/community/handlers/notification/mailer.rb',
+#    command => '/opt/sensu/embedded/bin/ruby /etc/sensu/handlers/notification/mailer.rb',
+    require => File['/etc/sensu/conf.d/mailer-conf.json'],
+  }
+
+  file {'/etc/sensu/conf.d/mailer-conf.json':
+    ensure => file,
+    owner  => 'sensu',
+    group  => 'sensu',
+    mode   => '0644',
+    content => template('sensu_server/mailer.json.erb'),
+    require   => Class['sensu_server::install'],
+  }
+
+  exec {'install-embedded-mail-gem':
+    command   => '/opt/sensu/embedded/bin/gem install mail',
+    unless    => '/opt/sensu/embedded/bin/gem list --local | /bin/grep -q mail',
+    logoutput => true,
+    require   => Class['sensu_server::install'],
+  }
+
+}
